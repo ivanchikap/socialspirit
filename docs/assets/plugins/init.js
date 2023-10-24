d42cb6f6291de4d0e69875ad4226fda8880fab4a
Іванчик Антон Петрович <ivanchikap@nsj.gov.ua>
2023-10-24 15:37:06 +0300

    alfa finish

--- docs/assets/plugins/init.js
+++ docs/assets/plugins/init.js
@@ -22,4 +22,21 @@ $(function() {
         currentSlider.slick("slickNext");
     });
 
+
+    $('.testimonial__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 577,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
+    });
 });
